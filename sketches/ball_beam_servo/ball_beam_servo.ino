f7cdb013c1f81bcce49d7b7cb7cc32a835f72c37
VincentYing <vincent@vincentying.com>
2024-03-06 21:06:14 -0800

    Added LCD, standardized pins, horizontal to 90

--- sketches/ball_beam_servo/ball_beam_servo.ino
+++ sketches/ball_beam_servo/ball_beam_servo.ino
@@ -1,9 +1,14 @@
 #include<Servo.h>
 #include<PID_v1.h>
+#include <LiquidCrystal.h>
+
+// LCD constants
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Sensor Pins
-const int trigPin = 7;
-const int echoPin = 6;
+const int trigPin = 14;
+const int echoPin = 15;
 
 // Moving Average
 const int numSamples = 5;
@@ -16,11 +21,24 @@ const int servoPin = 9;
 Servo myServo;
  
 // PID variables
+const int KpPin = A4;
+const int KiPin = A5;
+const int KdPin = A6;
+const int inputPin = A7;
+const int stateToggle = 19;
 float Kp = 2.5;
 float Ki = 0.0;
 float Kd = 1.1;
 double setPoint, input, output, servoOutput;                                       
 PID myPid(&input, &output, &setPoint, Kp, Ki, Kd, DIRECT);
+
+void lcdSetup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Balance and Float!");
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+}
 
 void movingAvgSetup() {
   Serial.println("Averaging Setup");
@@ -39,7 +57,7 @@ void sensorSetup() {
 void servoSetup() {
   Serial.println("Servo Setup");
   myServo.attach(servoPin); 
-  myServo.write(121);       // Set beam horizontal
+  myServo.write(90);       // Set beam horizontal
 }
 
 void pidSetup() {
@@ -47,12 +65,13 @@ void pidSetup() {
   input = getDistance();
   myPid.SetMode(AUTOMATIC);
   myPid.SetSampleTime(30);   // Changes from default of 100ms to 30ms
-  myPid.SetOutputLimits(-59,59);  // servo limits
+  myPid.SetOutputLimits(-90, 90);  // servo limits
 }
 
 void setup() {
   Serial.begin(9600);
 
+  lcdSetup();
   // movingAvgSetup();
   sensorSetup();
   servoSetup();
@@ -64,16 +83,11 @@ void loop() {
   input = getDistance();                                            
   
   myPid.Compute();
-  servoOutput = 121 - output; // horizontal + (-output)
+  servoOutput = 90 + output; // horizontal + (-output)
   myServo.write(servoOutput);
 
-  Serial.print(millis() / 1000);
-  Serial.print(", ");
-  Serial.print(input);
-  Serial.print(", ");
-  Serial.print(output);
-  Serial.print(", ");
-  Serial.println(servoOutput);
+  lcdPrint();
+  serialPrint();
   delay(500);
 }
       
@@ -98,4 +112,27 @@ float getDistance() {
   }
   averageDist = sum / numSamples;
   return averageDist;
+}
+
+void lcdPrint() {
+  lcd.setCursor(0, 0);
+  lcd.print("Dist/Kp/Ki/Kd ");
+  lcd.setCursor(0, 1);
+  lcd.print(input);
+  lcd.print("/");
+  lcd.print(Kp);
+  lcd.print("/");
+  lcd.print(Ki);
+  lcd.print("/");
+  lcd.print(Kd);
+}
+
+void serialPrint() {
+  Serial.print(millis() / 1000);
+  Serial.print(", ");
+  Serial.print(input);
+  Serial.print(", ");
+  Serial.print(output);
+  Serial.print(", ");
+  Serial.println(servoOutput);
 }
